a8de496ca56ab738d91dcb660e819817071cbbb9
Hervé Biey <herve.biey@gmail.com>
2023-12-30 21:00:01 +0100

    IMN-2: Implement the front page of the website.

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -1,10 +1,10 @@
 import clsx from 'clsx'
 
 export function Prose({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<'div'>) {
-  return (
-    <div className={clsx(className, 'prose dark:prose-invert')} {...props} />
-  )
+	                      className,
+	                      ...props
+                      }: React.ComponentPropsWithoutRef<'div'>) {
+	return (
+		<div className={clsx(className, 'prose dark:prose-invert')} {...props} />
+	)
 }
