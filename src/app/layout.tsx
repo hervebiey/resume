c0a4f44912627ae00e8a4a98fdce64f1aeeb2e78
Hervé Biey <herve.biey@gmail.com>
2024-01-04 02:47:30 +0100

    IMN-1: Added a new favicon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,15 @@ import {Providers} from '@/app/providers'
 import {Layout} from '@/components/Layout'
 
 import '@/styles/tailwind.css'
+import Head from "next/head";
 
-export const metadata: Metadata = {
+export const metadata = {
 	title: {
 		template: '%s - Hervé Biey',
-		default:
-			'Hervé Biey - Full-Stack Freelance Developer',
+		default: 'Hervé Biey - Full-Stack Freelance Developer',
 	},
 	description:
-		'I’m Hervé Biey, a Full-Stack Freelance Professional Developer with a Flair for Innovation.',
-	alternates: {
-		types: {
-			'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
-		},
-	},
+		'I’m Hervé Biey, a Full-Stack Freelance Professional Developer with a Flair for Innovation.'
 }
 
 export default function RootLayout({
@@ -27,6 +22,11 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className="h-full antialiased" suppressHydrationWarning>
+		<Head>
+			<title>{metadata.title.default}</title>
+			<meta name="description" content={metadata.description}/>
+			<link rel="icon" href="@/app/favicon.ico"/>
+		</Head>
 		<body className="flex h-full bg-zinc-50 dark:bg-black">
 		<Providers>
 			<div className="flex w-full">
